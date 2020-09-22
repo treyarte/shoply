f18ea5b0a31e06ac04d0d21bbfeacc0f8c51a771
Trey <tajohns22@gmail.com>
2020-09-22 17:51:09 -0500

    Updates

---
+++ precache-manifest.7a9a49b8d34d57e4f769d9957caf22dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "723f64c24c4e04752166739dfcf2a12d",
+    "url": "/shoply/index.html"
+  },
+  {
+    "revision": "bdc94c551e2e69abb75a",
+    "url": "/shoply/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "5e817015efb761d57ec4",
+    "url": "/shoply/static/css/main.aae606f5.chunk.css"
+  },
+  {
+    "revision": "bdc94c551e2e69abb75a",
+    "url": "/shoply/static/js/2.1fafe006.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shoply/static/js/2.1fafe006.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e817015efb761d57ec4",
+    "url": "/shoply/static/js/main.a83cf779.chunk.js"
+  },
+  {
+    "revision": "dc642e06ded8d3c31fc8",
+    "url": "/shoply/static/js/runtime-main.b0484728.js"
+  }
+]);
